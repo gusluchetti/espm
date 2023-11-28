d71fd8791d821e641f148bfcaced4f39b61cd681
Gustavo Luchetti <gustavo@luchetti.dev>
2023-11-28 19:53:15 -0300

    feat: mono repo for espm AI related subjects

---
+++ advanced-analytics/README.md
@@ -0,0 +1,3 @@
+# advanced-analytics
+
+Collection of all the exercises I made for my Advanced Analytics class @ ESPM 2021.2
---
+++ advanced-analytics/ex2/.ipynb_checkpoints/jupytext_conversion/Untitled-checkpoint.py
@@ -0,0 +1,17 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.0
+#   kernelspec:
+#     display_name: advanced-analytics
+#     language: python
+#     name: advanced-analytics
+# ---
+
+import os
+
+# # test
